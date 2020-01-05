502ed3b672c761921834bef357e530931d5f6ef5
sobanjawaid30 <sobanjawaid30@gmail.com>
2020-01-05 18:49:37 +0530

    mvc added

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
